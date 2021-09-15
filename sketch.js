2e8f1e6729f7caeda75bfaf1f09a322d61b3d511
Foxfire-13 <foxfire13.yt@gmail.com>
2021-09-15 15:09:54 +0100

    Sitema de tentativas

--- sketch.js
+++ sketch.js
@@ -3,22 +3,27 @@ var numeroSecreto = parseInt(Math.random() * 11);
 var tentativas = 4;
 
 function Chutar() {
-	var chute = parseInt(document.getElementById('valor').value);
-	var resultado = document.getElementById('resultado');
+	if (tentativas > 0) {
+		var chute = parseInt(document.getElementById('valor').value);
+		var resultado = document.getElementById('resultado');
 
-	//Usar dois == para comparar valores, apenas um = para atribuir valores
+		//Usar dois == para comparar valores, apenas um = para atribuir valores
 
-	if (chute == numeroSecreto) {
-		resultado.innerHTML = 'Você acertou!';
-	} else if (chute > 10 || chute < 0) {
-		resultado.innerHTML = 'Você deve escrever um número entre 0 e 10';
+		if (chute == numeroSecreto) {
+			resultado.innerHTML = 'Você acertou!';
+		} else if (chute > 10 || chute < 0) {
+			resultado.innerHTML = 'Você deve escrever um número entre 0 e 10';
+		} else {
+			resultado.innerHTML = 'Você errou!';
+			tentativas--;
+		}
 	} else {
-		resultado.innerHTML = 'Você errou! O número era ' + numeroSecreto;
+		var resultado = document.getElementById('resultado');
+		resultado.innerHTML = 'Você esgotou todas as tentativas!';
 	}
 }
 
 /*
--Colocar tentativas;
 -Mostrar se é maior ou menor
 -Colocar modo fácil, médio, díficil, hardcore
 */
