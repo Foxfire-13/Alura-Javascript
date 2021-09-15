66558fe79b8f418d61bd1cfd1d7a403ee28f428a
Foxfire-13 <foxfire13.yt@gmail.com>
2021-09-15 14:41:10 +0100

    Não compara numeros fora de limites

--- sketch.js
+++ sketch.js
@@ -3,4 +3,14 @@ var numeroSecreto = 3;
 function Chutar() {
 	var chute = parseInt(document.getElementById('valor').value);
 	console.log(chute);
+
+	//Usar dois == para comparar valores, apenas um = para atribuir valores
+
+	if (chute == numeroSecreto) {
+		console.log('Acertou!');
+	} else if (chute > 10 || chute < 0) {
+		console.log('Você deve digitar um número de 0 a 10');
+	} else {
+		console.log('Errou!');
+	}
 }
