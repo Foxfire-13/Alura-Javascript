de8d696380db14e20168ada22461ca1bb0ad0888
Foxfire-13 <foxfire13.yt@gmail.com>
2021-09-15 15:27:51 +0100

    Seleção de dificuldade

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 var numeroSecreto = parseInt(Math.random() * 11);
+var resultado = document.getElementById('resultado');
 
-var tentativas = 4;
+var tentativas = 7;
 
 function Chutar() {
 	if (tentativas > 0) {
 		var chute = parseInt(document.getElementById('valor').value);
-		var resultado = document.getElementById('resultado');
 
 		//Usar dois == para comparar valores, apenas um = para atribuir valores
 
 		if (chute == numeroSecreto) {
 			resultado.innerHTML = 'Você acertou!';
+			numeroSecreto = parseInt(Math.random() * 11);
 		} else if (chute > 10 || chute < 0) {
 			resultado.innerHTML = 'Você deve escrever um número entre 0 e 10';
 		} else {
@@ -18,12 +19,12 @@ function Chutar() {
 			tentativas--;
 		}
 	} else {
-		var resultado = document.getElementById('resultado');
 		resultado.innerHTML = 'Você esgotou todas as tentativas!';
 	}
 }
 
-/*
--Mostrar se é maior ou menor
--Colocar modo fácil, médio, díficil, hardcore
-*/
+function AtualizaDificuldade() {
+	var elementoTentativas = document.getElementById('modo');
+	tentativas = parseInt(elementoTentativas.value);
+	resultado.innerHTML = '';
+}
